ac5a71af8c1f85044feb045fe66ce995077a5b4c
JohnTarvis <3378529@gmail.com>
2020-02-17 15:29:40 -0500

    Update define.js

--- define.js
+++ define.js
@@ -1,6 +1,8 @@
 "use strict";
 
-(function() {
+
+
+(function(){
   /**
    * Check and set a global guard variable.
    * If this content script is injected into the same page again,
@@ -10,135 +12,55 @@
 		return;
 	}
 	window.hasRun = true;
-  
-/*   */
-  ///////////////////////////////////////////////////////////////////////////////
-  
-  //add jquery   
+    
+    
+    /////////////////////////////////////////////////////////////////////
+    
+    
+    let selectedID = 'defineTooltip';
+    let selectedClass = 'tooltipClass';
+	let lastSelected = '';
 
-
-
-
-  
-	let InfoPane = {	    
-	  selectedText: 'carrots',  
-	  x: 0,
-	  y: 0,
-	  iframe: document.createElement('iframe'),
-	  search_dictionary_COM(){
-		  return "https://www.dictionary.com/browse/" + this.selectedText;
-	  },		  
-	  getFirstElement(){
-		  return document.body.childNodes[0];
-	  },	  
-	  setupButtonStrip(){
-		  return '<button class="testbtn">test</button>'		  
-	  },
-	  setup_iframe(){
-		  this.iframe.className = 'infoPane';
-		  this.iframe.style.backgroundColor = 'white';
-		  this.iframe.style.fontSize = 'large';
-		  this.iframe.style.color = 'red';
-		  this.iframe.style.position = 'fixed';	  
-		  this.iframe.style.top = this.y + "px";
-		  this.iframe.style.left = this.x + "px";	  
-		  this.iframe.style.width = '50%';
-		  this.iframe.style.height = '25%';
-		  this.iframe.style.zIndex = "999";		  
-		  this.iframe.setAttribute('src',this.search_dictionary_COM());	  
-	  },	  
-	  displayPane(){
-		  this.remove();
-		  this.setup_iframe();
-		  document.body.insertBefore(this.iframe, this.getFirstElement());
-	  },  	
-	  displayButtonStrip(){
-		  let buttons = document.querySelectorAll('.testbtn');
-		  for(let button of buttons){
-			  button.remove();
-		  }
-		  document.body.insertBefore(this.setupButtonStrip(),this.getFirstElement());
-	  },
-	  remove(){
-		  let panes = document.querySelectorAll('.infoPane');
-		  for(let pane of panes){
-			  pane.remove();
-		  }
-	  }	  
-  }; 
-
-/////////////////////////////////////////////////////////////////////////////// 
-
-	let lastSelected;
-	
-	function highlightAndWrap(){		
-		let highlightedWord = '';
-		let selection = window.getSelection()
-		let selectionRange = selection.getRangeAt(0);
-		let selectionText;
-		
-		if(typeof selection){
-			selectedText = selection.toString();
-		} else if (typeof document.selection && document.selection.type == 'Text') {
-			selection = document.selection.createRange();
-			selectionText = document.selection.createRange().text;
-		}
-		lastSelected = selectedText;		
-	}
-  
-	function getHighlightedText(mouseEvent) {	  
-		let text = "";
-		let selection;
-		if (typeof window.getSelection){
-			selection = window.getSelection();
-			text = window.getSelection().toString();      
-		} else if (typeof document.selection && document.selection.type == "Text") {
-			selection = document.selection.createRange();
-			text = document.selection.createRange().text;
-		}
-		lastSelected = text;		
-		
-	}	
-  
-	function wrapSelectedText() {
-		
-		let selection= window.getSelection().getRangeAt(0);
-		let selectedText = selection.extractContents();	
-		let span = document.createElement("span");
-		span.style.backgroundColor = "yellow";
-		span.appendChild(selectedText);
-		span.id = 'defineTooltip'		
-		selection.insertNode(span);
-		
-		
-	}
-
-	 
-	
+    function testStuff(){
+        let selection = window.getSelection().getRangeAt(0);
+		let selectedText = selection.extractContents();
+        let selectionString = window.getSelection().toString;
+        let rangeText = document.selection.createRange().text;
+        if(selectedText != ''){
+            removeWrap();
+            let span = document.createElement("span");
+            span.style.backgroundColor = "yellow";
+            span.appendChild(selectedText);
+            span.id = selectedID;
+            selection.insertNode(span);
+            lastSelected = span.innerHTML;
+            
+            alert('selectionText = ' + selectedText + '\n selectionString = ' + selectedText);
+            //alert(lastSelected);
+        }
+    }
+    
+    function sleep(mils){
+        let currentTime = new Date().getTime();
+        while(currentTime + mils >= new Date().getTime()){}
+    }
+    
+///////////////////////////////////////////////////////////////////////////////////////
 	function removeWrap(){
-		let wrap = document.getElementById('defineTooltip');			
-		if(wrap && lastSelected){
-			wrap.parentNode.replaceChild(document.createTextNode(lastSelected), wrap);		
-			
-		}
+        if(lastSelected != ''){
+            let defineTooltip = document.getElementById(selectedID);
+            defineTooltip.insertAdjacentHTML('beforebegin',lastSelected);
+            defineTooltip.parentNode.removeChild(defineTooltip);
+            lastSelected = '';
+        }
 	}
-	
-	
-
-  ///////////////////////////////////////////////////////////////////////////////
   ///////////////////////////////////////////////////////////////////////////////
   (function runningDefine(){
-	document.onmouseup = getHighlightedText;//wrapSelectedText;
-	document.onkeyup = wrapSelectedText;	
-	document.onmousedown = removeWrap;	
+	document.onmouseup = testStuff;
+	document.onkeyup = testStuff;	
   })();
   ///////////////////////////////////////////////////////////////////////////////
-  ///////////////////////////////////////////////////////////////////////////////
-	
 
-	
-	
-  ////////////////////////////////////////////////////////////////////////////////
 })();
 
 
@@ -149,43 +71,17 @@
 *`````````````````````````````````````````````````````````````````
 *
 
-		if(text){
-			InfoPane.selectedText = text;
-			if (mouseEvent){
-				InfoPane.x = mouseEvent.clientX;
-				InfoPane.y = mouseEvent.clientY;
-			}
-		} 
-
-	function XremoveWrap(){
-		let wraps = document.querySelectorAll('.wrap');
-		for(let wrap of wraps){
-			//wrap.outerHTML = wrap.innerHTML;
-			wrap.insertBefore(lastSelected);
-			wrap.remove();
-			
-		}
-	}
-
-
-	function temp_remove(){
-		let panes = document.querySelectorAll('.infoPane');
-		for(let pane of panes){
-		  pane.remove();
-		}
-	}	 
-  
-  	function yellowHighlighted(mouseEvent) {
-		if (window.getSelection) {
-			var sel = window.getSelection();
-			if (sel.rangeCount) {
-				var range = sel.getRangeAt(0).cloneRange();
-				range.surroundContents('<b>');
-				sel.removeAllRanges();
-				sel.addRange(range);
-			}
-		}
-	}
+    function highlightSelection(mouseEvent){ 
+        let selection = window.getSelection().getRangeAt(0);
+        let selectedText = selection.toString();
+        let selectionFragment = selection.extractContents();
+        let span = document.createElement('span');
+        span.style.backgroundColor = 'green';
+        span.appendChild(selectionFragment);
+        span.id = 'greenHighlight';
+        selection.insertNode(span);
+        lastHTML = span.innerHTML;
+    }
   
 	////////////////////////////6666666666666666666666666666666666666	
 	////////////////////////////777777777777777777777777777	
