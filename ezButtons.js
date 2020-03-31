085ba29e94ce8a38d53b60dd40cee7442b6b5834
JohnTarvis <3378529@gmail.com>
2020-03-31 16:21:01 -0400

    Update ezButtons.js

--- ezButtons.js
+++ ezButtons.js
@@ -1,37 +1,73 @@
 "use strict";
-
+// &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@&/@@@@@@@&(&@@@%%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&@@@@%&@@@@@@@@%@@@@
+// @@@@@&%,%&&&&&##,&&&,(@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*&@@#(%@@@@@@(#@@@@@
+// @@@@@&#%((/%&&%(,(%&,#*&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%/%*&*##,&&&#,*&@@&@@@
+// @@@@@@%**(#%%(*#%/#%(#(/%@@@@@@@@@@@@&#&@@@@@@@@@@@@@@@@&*/*((/*(##/#((&/*#@@@@@
+// @@@@@(/&#,/((,/#%%%,,,#%/#*&@@@@@@@@&//%/@@@@@@@@@@@@((,(,#*/(#%%%//(/%/#@&&&@@@
+// @@@@@@&//(#///*#&%((%%%&%%*/(&&@@@@@&((#*/(#&@@@@@@@&///##%&&%/#%%//,/#%*/&@@@@@
+// @@@&&&#*(/*////##%%%#((%%(#(///%&@@@&#/%&%/&@&&&&&#,(#(//#%#/##%%(#(*/,//*(%@@@@
+// @@@@@@@@@&&%%%%/##(%#&%#(#(##(#/(*/*/*/#%#(*/./**(##(##(**%&&#%%%#/#%%&&@@@@@@@@
+// @@@@@@@&@&#(*%%#(##%%%#(&#(#%%#/(#(/(*#(#,##*(/%(/,/#((%%&/%%%*(/#%%(((&@@@@@@@@
+// @@@@@@@@/(&&&&#*#&%(%##%##%#%((%%&#(#/((%#((,((#/#%%,#/(/*/(%(#&(/*(@@@&%(@@@@@@
+// @@@@@@&@@@@@%,%,%/%%/%%((%(*(%#%#&&&&/#%###/&&&&&%/#(/(##(/(,*%%*#*.&%@@@@@@@@@@
+// @@@@@@@@@@@&*&&&*%@%/#(&@(@/%@&&@@@&&/((/(%#&&@@@@&@@&#@%@&/&(#(%/@&@(@@@@@@@@@@
+// @@@@@@@@%%@@@&@&@@@@@@&@@@@@&@@@@@(//(#/(**#(##@@@@@@@@@@@@@&&&&@&&&@@@@@@@@@@@@
+// @@@@@@&@@@@@@@@@@@@@@@@@@@@@@@@@&(#%#(#((#/##(&/&@@@@@@@@@@@@@@@@@@&%@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%&&&/(/%(#%#(#&(%@@&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&/(/#((/(####(/%#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%%@/(&#%&*#(&/(@&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%&@@@,&@@/&@@@@&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 ///_____________________________________________________________________|~
 //--------------------------------|`````````|---------------------------|~~
 //--------------------------------|CONSTANTS|---------------------------|~~~
 //--------------------------------|_________|---------------------------|~~
 ///`````````````````````````````````````````````````````````````````````|~
-
 const ezLibrary_id = "ezLibrary_id";
 const copyButton_id = 'copyButton_id';
 const copyButtonImageURL = "url('icons/copyButtonIcon21-26.png')";
-
 ///_____________________________________________________________________|~
 //--------------------------------|`````````|---------------------------|~~
 //--------------------------------|VARIABLES|---------------------------|~~~
 //--------------------------------|_________|---------------------------|~~
 ///`````````````````````````````````````````````````````````````````````|~
-
 let ezLayout;
-
 ///_____________________________________________________________________|~
 //--------------------------------|```````|-----------------------------|~~
 //--------------------------------|OBJECTS|-----------------------------|~~~
 //--------------------------------|_______|-----------------------------|~~
 ///`````````````````````````````````````````````````````````````````````|~
-
-
 ///_____________________________________________________________________|~
 //--------------------------------|`````````|---------------------------|~~
 //--------------------------------|FUNCTIONS|---------------------------|~~~
 //--------------------------------|_________|---------------------------|~~
 ///`````````````````````````````````````````````````````````````````````|~
-
-async function loadScript(src, id) {
+async function loadStyle(href, id) {
+    if(!!document.getElementById(id)) {
+        console.log("removing duplicate script: " + href);
+        let duplicate = document.getElementById(id);
+        duplicate.parentElement.removeChild(duplicate);
+    }
+    
+    let promise = new Promise(function(resolve, reject) {
+        let link = document.createElement("link");
+        link.href = href;
+        link.async = false;
+        link.onload = () => resolve(link);
+        link.onerror = () => reject(new Error(`stylesheet did not load because ${href}`));
+        link.type = "text/css";
+        link.rel = "stylesheet";
+        document.head.append(link);
+    });
+    
+    promise.then(
+        link => console.log(`loaded: ${link.href}`),
+        error => console.log(`Error: ${error.message}`)
+    );
+}
+async function loadScript(src, id, first = false) {
     if(document.getElementById(id)) {
         console.log("removing duplicate script: " + src);
         let duplicate = document.getElementById(ezLibrary_id);
@@ -46,7 +82,12 @@ async function loadScript(src, id) {
         
         script.onload = () => resolve(script);
         script.onerror = () => reject(new Error(`Script load error for ${src}`));
-        document.head.append(script);
+		
+		if(!!first) {
+			document.head.insertBefore(script,document.head.firstChild);
+		} else {
+			document.head.append(script);
+		}
     });
     
     promise.then(
@@ -54,21 +95,18 @@ async function loadScript(src, id) {
         error => console.log(`Error: ${error.message}`)
     );
 }
-
 ///_____________________________________________________________________|~
 //--------------------------------|````|--------------------------------|~~
 //--------------------------------|MAIN|--------------------------------|~~~
 //--------------------------------|____|--------------------------------|~~
 ///`````````````````````````````````````````````````````````````````````|~
-
 function makeButton(){
     let button = document.createElement("button");
-    
     button.style.width = "12px";
     button.style.height = "12px";
     button.style.position = "absolute";
     //button.style.top = "50%";
-    button.style.left = "57%";
+    //button.style.left = "57%";
     button.style.transform = "translate(-50%,-50%)";
     button.style.msTransform = "translate(-50%,-50%)";
     button.style.backgroundColor = "#555";
@@ -89,84 +127,231 @@ function makeButton(){
     
     return button;
 }
-
 function makeContainer() {
     
-    let container = document.createElement("div");
-    
+    let container = document.createElement("p");
     container.style.position = "relative";
-    
     container.style.width = "50%";
-    
+    container.id = "ezContainer";
     return container;
 }
-
 function mu(){
+	
+	ez.test();
     
     let images = document.querySelectorAll("img");
-   
-	//let image;
-	
-    // for( image in images) {
-
-        // let button = makeButton();
-
-        // let container = makeContainer();
-
-        // container.appendChild(button);
-
-        // container.appendChild(image);
-
-        // document.body.appendChild(container);
-
-        // image.parentElement.replaceChild(container,image);
-        
-    // }
-	
 	let count = 0;
-	for(count = 0; count < images.length; count++) {
+	
+	for(let count = 0; count < images.length; count++) {
 		let image = images[count];
 		
-		let button = makeButton();
-
-        let container = makeContainer();
-
-        container.appendChild(button);
-
-        container.appendChild(image);
+		image.ezImageTag = "ezImageTag";
 		
-		//image.parentElement.insertAdjacentHTML("beforebegin", container.outerHTML);
-
-        //document.body.appendChild(container);
-
-        //document.body.replaceChild(container, image);
 		
-		//l_(image.id);
+		
+        if(image.parentElement.id == "ezContainer") {
+            
+            
+            
+        } else {
+            
+            image.parentElement.insertBefore(container,image);
+            container.appendChild(image);
+            container.appendChild(button);
+        }
+        
+        
+        
 	}
-
 }
-
 function mu2() {
-    l_("nothing here");
+    l_("mu2");
+	
+	
+
+	
+	
+	
+	
+}
+
+async function load_jQuery() {
+	var script = document.createElement('script'); 
+	document.head.appendChild(script);    
+	script.type = 'text/javascript';
+	script.src = "jQuery.js";
+	await script.onload
+}
+
+function mu3() {
+	
+    let contextMenu = document.createElement("ul");
+		contextMenu.className = "custom-menu";
+		
+		let listItem = document.createElement("li");
+		listItem.data = "first";
+		listItem.innerText = "proceed";
+		
+		let listItem2 = document.createElement("li");
+		listItem2.data = "second";
+		listItem2.innerText = "exit";
+		
+		contextMenu.appendChild(listItem);
+		contextMenu.appendChild(listItem2);
+		
+		document.body.insertBefore(contextMenu,document.body.childNodes[0]);
+		//document.body.appendChild(contextMenu);
+		
+			
+		$("img").css("border","5px solid orange");
+		
+		// JAVASCRIPT (jQuery)
+
+		// Trigger action when the contexmenu is about to be shown
+		$(document).bind("contextmenu", function (event) {
+			
+			// Avoid the real one
+			event.preventDefault();
+			
+			// Show contextmenu
+			$(".custom-menu").finish().toggle(100).
+			
+			// In the right position (the mouse)
+			css({
+				top: event.pageY + "px",
+				left: event.pageX + "px"
+			});
+		});
+
+
+		// If the document is clicked somewhere
+		$(document).bind("mousedown", function (e) {
+			
+			// If the clicked element is not the menu
+			if (!$(e.target).parents(".custom-menu").length > 0) {
+				
+				// Hide it
+				$(".custom-menu").hide(100);
+			}
+		});
+
+
+		// If the menu element is clicked
+		$(".custom-menu li").click(function(){
+			
+			// This is the triggered action name
+			switch($(this).attr("data-action")) {
+				
+				// A case for each action. Your actions here
+				case "first": alert("first"); break;
+				case "second": alert("second"); break;
+				case "third": alert("third"); break;
+			}
+		  
+			// Hide it AFTER the action was triggered
+			$(".custom-menu").hide(100);
+		  });
+	
+	
+}
+
+function getHighestZindex() {
+    let children = $("body").children();
+    let highestZ = -999;
+    for(let count = 0; count < children.length; count++) {
+        if(children[count].style.zIndex > highestZ)
+            highestZ = children[count].style.zIndex;
+    }
+    return highestZ
+}
+
+function createElement (type = "div") {
+    let element = document.createElement(type);
+    return element;    
+}
+
+function createImageToSave (src) {
+    
+    let image = document.createElement("img");
+    
+    image.src = src;//"https://images-na.ssl-images-amazon.com/images/I/7102BnBdGoL._AC_UX679_.jpg";
+    
+    image.style.maxHeight = "90%";
+
+    image.style.maxWidth = "90%";
+
+    image.style.border = "2px solid gold";
+
+    image.style.margin = "5px";
+
+    image.style.display = "inline-block";
+    
+    return image;
+    
 }
 
 
-(async function main(){
+
+
+(async function main() {
 	if (window.hasRun) {
 		return;
 	}
 	window.hasRun = true;
+	
+	await loadScript("jQuery.js", "jQuery_id", true);
+	
+	$(document).ready(function(){
+		
+        ///-------------------------------------------------------------------
+        ///-------------------------------------------------------------------	
 
-    await loadScript("../scripts/ezLibrary.js");
-    
-    document.onmouseup = mu;
-    
-    //mu();
+        //let div = createElement();
+        
+        let div = document.createElement("div");
+        
+        div.style.backgroundColor = "green";
 
+        div.style.height = "auto";
+        
+        div.style.width = "100px";
+                
+        //div.style.position = "absolute";
+        
+        let firstChild = document.body.firstChild;
+        
+        div.style.zIndex = "999";
+        
+        div.style.textAlign = "center";
+        
+        div.style.position = "fixed";
+        
+        div.style.overflow = "auto";
+        
+        div.id = "tempDiv";
+        
+        if(!!document.getElementById("tempDiv")) {
+            dupDiv = document.getElementById("tempDiv");
+            dupDiv.parentElement.removeChild(dupDiv);
+        }
+        
+        document.body.insertBefore(div,firstChild);
+        
+        let images = document.querySelectorAll("img");
+        
+        for(let count = 0; count < images.length; count++) {
+            let rawImageSource = images[count].src;
+            let imageToSave = createImageToSave(rawImageSource);
+            div.appendChild(imageToSave);
+        }
+        ///--------------------------------------------------------------------	
+        ///--------------------------------------------------------------------
+		
+    });
+	
+    //document.onmouseup = mu2;
+    
 })();
-
-
-
 ///________________________________________________________________________|~
 //--------------------------------|````|-----------------------------------|~~
 //--------------------------------|TEST|-----------------------------------|~~~
@@ -174,8 +359,6 @@ function mu2() {
 ///````````````````````````````````````````````````````````````````````````|~
 
 
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ///_________________________________________________________________________|~
 //--------------------------------|````````|--------------------------------|~~
